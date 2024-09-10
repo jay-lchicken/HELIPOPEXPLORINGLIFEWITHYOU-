fc3adc3134842bda43be897fc1078c30313bab1d
Lai Hong Yu <joyjay606@gmail.com>
2024-09-10 15:46:46 +0800

    Initial Commit

---
+++ INCINCINC/INCINCINCApp.swift
@@ -0,0 +1,17 @@
+//
+//  INCINCINCApp.swift
+//  INCINCINC
+//
+//  Created by Lai Hong Yu on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct INCINCINCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
