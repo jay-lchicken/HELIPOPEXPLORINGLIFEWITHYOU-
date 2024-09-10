3dddfd95c874afd21d61f1fdcf80471ee3548ff5
Lai Hong Yu <joyjay606@gmail.com>
2024-09-10 19:15:56 +0800

    Using DataSave

--- INCINCINC/ViewModels/LogManagementViewModel.swift
+++ INCINCINC/ViewModels/LogManagementViewModel.swift
@@ -7,31 +7,32 @@
 import CoreLocation
 import Foundation
 import SwiftUICore
+import DataSave
 class LogManagementViewModel: ObservableObject {
     @Published var fontSize: ContentSizeCategory = .large
     @Published var logs: [log] = []
     private let key = "announcementList"
     init() {
-        if let data = UserDefaults.standard.data(forKey: key) {
-            let decoder = JSONDecoder()
-            if let decodedList = try? decoder.decode([log].self, from: data) {
-                self.logs = decodedList
-            }
+//        if let data = UserDefaults.standard.data(forKey: key) {
+//            let decoder = JSONDecoder()
+//            if let decodedList = try? decoder.decode([log].self, from: data) {
+//                self.logs = decodedList
+//            }
+//        }
+        if let logss: [log] = DataSave.retrieveFromUserDefaults(forKey: key, as: [log].self) {
+            logs = logss
+        } else {
+            print("No data found for the given key.")
         }
     }
     func addData(logging: log){
         logs.append(logging)
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(logs) {
-                    UserDefaults.standard.set(encoded, forKey: key)
-            }
+        let success = DataSave.saveToUserDefaults(logs, forKey: key)
+        
     }
     func deleteData(index: Int){
         logs.remove(at: index)
-        let encoder = JSONEncoder()
-        if let encoded = try? encoder.encode(logs) {
-                    UserDefaults.standard.set(encoded, forKey: key)
-            }
+        let success = DataSave.saveToUserDefaults(logs, forKey: key)
     }
     func add(name: String, why: String, how: String, locationFledd: CLLocationCoordinate2D, locationNoww: CLLocationCoordinate2D){
         let currentData = log(nameOfAccomplice: name, locationFled: log.LocationData(from: CLLocation(latitude: locationFledd.latitude, longitude: locationFledd.longitude)), dateAdded: Date().timeIntervalSince1970, whyStolen: why, whereAreTheyNow: log.LocationData(from: CLLocation(latitude: locationNoww.latitude, longitude: locationNoww.longitude)), howTheyStoleTheCoin: how)
